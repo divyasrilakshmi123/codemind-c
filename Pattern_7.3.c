43b5b71a751b8e182d7759d02fc20dddfc26300a
divyasrilakshmi123 <147492029+divyasrilakshmi123@users.noreply.github.com>
2023-12-31 01:34:51 -0800

    #include<stdio.h> int main() {     int i,j,n,k;     scanf("%d",&n);     for(i=n;i>=1;i--)     {         for(k=n-1;k>=i;k--)         {             printf(" ");         }         for(j=i;j>=1;j--)         {             printf("%d ",j);         }         printf(" ");     }     return 0; }

---
+++ Pattern_7.3.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,n,k;
+    scanf("%d",&n);
+    for(i=n;i>=1;i--)
+    {
+        for(k=n-1;k>=i;k--)
+        {
+            printf(" ");
+        }
+        for(j=i;j>=1;j--)
+        {
+            printf("%d ",j);
+        }
+        printf("
+");
+    }
+    return 0;
+}
